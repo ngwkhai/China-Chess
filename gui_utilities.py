25f427f257281087973170ea900a88c3b7351c70
TaGiangThuyLoan <23020397@vnu.edu.vn>
2024-10-02 08:56:03 +0700

    Create check_for_input & change_color in class Button

--- gui_utilities.py
+++ gui_utilities.py
@@ -20,3 +20,16 @@ class Button():
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
+    def check_for_input(self, position):
+        return (
+            position[0] in range(self.rect.left, self.rect.right)
+            and position[1] in range(self.rect.top, self.rect.bottom)
+        )
+
+    def change_color(self, position):
+        if self.check_for_input(position):
+            self.text = self.font.render(
+                self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(
+                self.text_input, True, self.base_color)
