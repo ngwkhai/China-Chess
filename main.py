eb59cc1a398033df94abe21a3670f7f8092c1622
Nguyen Dinh Khai <166140280+ngwkhai@users.noreply.github.com>
2024-10-07 10:19:19 +0700

    Add eve menu base for main function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 import pygame
 import resource
-from gui_utilities import Button
+from gui_utilities import Button, InputBox
 
 # Khởi tạo pygame
 pygame.init()
@@ -17,6 +17,96 @@ REFRESH_RATE = 30
 
 # Tạo đối tượng đồng hồ
 clock = pygame.time.Clock()
+
+def eve_menu():
+    # Tạo một số tiện ích
+    # black_type = DropDown(
+    #     ["#000000", "#202020"],
+    #     ["#404040", "#606060"],
+    #     20, 290, 100, 30,
+    #     pygame.font.SysFont(None, 25),
+    #     "Bot", ["Minimax", "MCTS"]
+    # )
+    # black_value = DropDown(
+    #     ["#000000", "#202020"],
+    #     ["#404040", "#606060"],
+    #     180, 290, 100, 30,
+    #     pygame.font.SysFont(None, 25),
+    #     "Cấp độ", ["0", "1", "2"]
+    # )
+    # red_type = DropDown(
+    #     ["#DC1C13", "#EA4C46"],
+    #     ["#F07470", "#F1959B"],
+    #     350, 290, 100, 30,
+    #     pygame.font.SysFont(None, 25),
+    #     "Bot", ["Minimax", "MCTS"]
+    # )
+    # red_value = DropDown(
+    #     ["#DC1C13", "#EA4C46"],
+    #     ["#F07470", "#F1959B"],
+    #     510, 290, 100, 30,
+    #     pygame.font.SysFont(None, 25),
+    #     "Cấp độ", ["0", "1", "2"]
+    # )
+
+    num_box = InputBox(330, 125, 40, 40, pygame.font.SysFont(None, 35),
+                       "Black", "Red", "Số trận")
+    black_another_property = InputBox(165, 230, 40, 30, pygame.font.SysFont(
+        None, 25), "Black", "Red", "Độ sâu cho chép")
+    red_another_property = InputBox(495, 230, 40, 30, pygame.font.SysFont(
+        None, 25), "Red", "Black", "Độ sâu cho chép")
+
+    start_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 450),
+                          text_input="Bắt đầu", font=resource.get_font(40, 0), base_color="#AB001B", hovering_color="Black")
+    quit_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(165, 550),
+                         text_input="Thoát", font=resource.get_font(30, 0), base_color="Black", hovering_color="#AB001B")
+    back_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(495, 550),
+                         text_input="Trở lại", font=resource.get_font(30, 0), base_color="Black", hovering_color="#AB001B")
+
+    # Bắt đầu vòng lặp trò chơi
+    while True:
+        # Nhận trạng thái trò chơi hiện tại
+        mouse_pos = pygame.mouse.get_pos()
+        events = pygame.event.get()
+
+        # Vẽ menu chính
+        # Vẽ nền
+        bg_img, bg_pos = resource.background()
+        SCREEN.blit(bg_img, bg_pos)
+
+        # Vẽ chữ
+        menu_text = resource.get_font(70, 0).render("Chọn Bots", True, "Black")
+        menu_rect = menu_text.get_rect(center=(330.5, 60))
+        SCREEN.blit(menu_text, menu_rect)
+
+        text = resource.get_font(60, 0).render("Black", True, "Black")
+        rect = text.get_rect(center=(165, 185))
+        SCREEN.blit(text, rect)
+
+        text = resource.get_font(30, 0).render("Kiểu Bot", True, "Black")
+        rect = text.get_rect(center=(70, 270))
+        SCREEN.blit(text, rect)
+
+        text = resource.get_font(30, 0).render("Cấp độ", True, "Black")
+        rect = text.get_rect(center=(230, 270))
+        SCREEN.blit(text, rect)
+
+        text = resource.get_font(60, 0).render("Red", True, "#AB001B")
+        rect = text.get_rect(center=(495, 185))
+        SCREEN.blit(text, rect)
+
+        text = resource.get_font(30, 0).render("Kiểu Bot", True, "#AB001B")
+        rect = text.get_rect(center=(400, 270))
+        SCREEN.blit(text, rect)
+
+        text = resource.get_font(30, 0).render("Cấp độ", True, "#AB001B")
+        rect = text.get_rect(center=(560, 270))
+        SCREEN.blit(text, rect)
+
+        # Cập nhật màn hình
+        pygame.display.flip()
+        # Thời gian khung hình
+        clock.tick(REFRESH_RATE)
 
 def main_menu():
     """Hàm này là màn hình menu chính"""
@@ -52,7 +142,7 @@ def main_menu():
         for event in events_list:
             # Thoát khỏi trò chơi nếu nút QUIT được nhấp
             if event.type == pygame.QUIT:
-                print("")
+                print("Thoát")
                 pygame.quit()
                 sys.exit()
 
@@ -63,8 +153,9 @@ def main_menu():
                 if pvp_button.check_for_input(mouse_pos):
                     pass
                 if eve_button.check_for_input(mouse_pos):
-                    pass
+                    eve_menu()
                 if quit_button.check_for_input(mouse_pos):
+                    print("Thoát")
                     pygame.quit()
                     sys.exit()
 
