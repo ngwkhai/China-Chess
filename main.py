785d17659af376bee0b04ba0acdc5a496aada390
Nguyen Dinh Khai <166140280+ngwkhai@users.noreply.github.com>
2024-10-12 09:48:08 +0700

    Fix comment and struct function in main

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import os
 import sys
 import gc
 from time import time
 import threading
-import pygame
-import resource
-from game_state import GameState
-from team import Team
 from cmath import inf
+import pygame
+from concurrent.futures import ProcessPoolExecutor
+import resource
+from piece import Piece
+from team import Team
+from game_state import GameState
 from gui_utilities import Button, InputBox, DropDown
 from game_tree import GameTree, GameTreeMinimax, GameTreeDynamicMinimax, GameTreeDeepeningMinimax
-from piece import Piece
+
+# Giới hạn bộ nhớ
+os.environ['PYPY_GC_MAX_DELTA'] = '3000 MB'
+os.environ['PYPY_GC_MAX_EXTERNAL_SIZE'] = '1000 MB'
 
 # Tạo các biến toàn cục
 # chứa nước đi và giá trị trạng thái trò chơi được giải quyết trong chuỗi chạy bot
@@ -570,63 +576,6 @@ def eve_menu():
         # Thời gian khung hình
         clock.tick(REFRESH_RATE)
 
-def main_menu():
-    """Hàm này là màn hình menu chính"""
-    # Tạo các nút
-    pve_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 220),
-                        text_input="Người vs Máy", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
-    pvp_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 330),
-                        text_input="Người vs Người", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
-    eve_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 440),
-                        text_input="Máy vs Máy", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
-    quit_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(330.5, 550),
-                         text_input="Thoát", font=resource.get_font(28, 0), base_color="Black", hovering_color="#AB001B")
-    # Bắt đầu vòng lặp game
-    while True:
-        # Nhận trạng thái trò chơi hiện tại
-        mouse_pos = pygame.mouse.get_pos()
-        events_list = pygame.event.get()
-
-        # Vẽ menu chính
-        # Vẽ background
-        bg_img, bg_pos = resource.background()
-        SCREEN.blit(bg_img, bg_pos)
-
-        # Vẽ chữ menu
-        menu_text = resource.get_font(100, 0).render("CỜ TƯỚNG", True, "White")
-        menu_rect = menu_text.get_rect(center = (330.5, 100))
-        SCREEN.blit(menu_text, menu_rect)
-
-        for button in [pve_button, pvp_button, eve_button, quit_button]:
-            button.draw(SCREEN)
-
-        # xử lý sự kiện
-        for event in events_list:
-            # Thoát khỏi trò chơi nếu nút QUIT được nhấp
-            if event.type == pygame.QUIT:
-                print("Thoát")
-                pygame.quit()
-                sys.exit()
-
-            # Xử lý sự kiện bấm vào các nút
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if pve_button.check_for_input(mouse_pos):
-                    pve_menu()
-                if pvp_button.check_for_input(mouse_pos):
-                    pvp_menu()
-                if eve_button.check_for_input(mouse_pos):
-                    eve_menu()
-                if quit_button.check_for_input(mouse_pos):
-                    print("Thoát")
-                    pygame.quit()
-                    sys.exit()
-
-        # Cập nhật màn hình
-        pygame.display.flip()
-
-        # Chờ khung hình tiếp theo
-        clock.tick(REFRESH_RATE)
-
 def pve_screen(
     bot_type: GameTree,
     bot_value: int,
@@ -646,7 +595,7 @@ def pve_screen(
     position_chosen, piece_chosen = None, None
     last_move = None
 
-    #
+    # Tạo nút
     quit_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(165, 530),
                          text_input="Thoát", font=resource.get_font(30, 0), base_color="Black", hovering_color="#AB001B")
 
@@ -821,18 +770,17 @@ def pve_menu():
     back_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(495, 530),
                          text_input="Trở lại", font=resource.get_font(30, 0), base_color="Black", hovering_color="#AB001B")
     
-        # Start the game loop
+    # Bắt đầu vòng lặp game
     while True:
-        # Get the current game status
+        # Lấy trạng thái trò chơi hiện tại
         mouse_pos = pygame.mouse.get_pos()
         events_list = pygame.event.get()
 
-        # Draw
-        # .Background
+        # Vẽ nền
         bg_img, bg_pos = resource.background()
         SCREEN.blit(bg_img, bg_pos)
 
-        # .Text
+        # .Viết Chữ
         menu_text = resource.get_font(70, 0).render("Nhập cuộc", True, "Black")
         menu_rect = menu_text.get_rect(center=(330.5, 60))
         SCREEN.blit(menu_text, menu_rect)
@@ -853,11 +801,11 @@ def pve_menu():
         rect = text.get_rect(center=(495, 165))
         SCREEN.blit(text, rect)
 
-        # .Button
+        # Vẽ nút
         for button in [start_button, quit_button, back_button]:
             button.draw(SCREEN)
 
-        # .List
+        # Vẽ danh sách
         for lst in [bot_type, bot_value, team_select]:
             selected_option = lst.update(events_list)
             if selected_option >= 0:
@@ -865,19 +813,19 @@ def pve_menu():
 
             lst.draw(SCREEN)
 
-        # .Input box
+        # Vẽ hộp nhập
         for input_box in [bot_another_property]:
             input_box.update()
             input_box.draw(SCREEN)
 
-        # Handle events
+        # Xử lý sự kiện kích chuột
         for event in events_list:
-            # Quit the game if the exit button is clicked
+            # Thoát trò chơi
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-            # Handle the button click event
+            # Xử lý khi bấm vào nút
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if start_button.check_for_input(mouse_pos):
                     if (
@@ -990,6 +938,8 @@ def pvp_menu():
         pygame.display.flip()
         # Thời gian khung hình
         clock.tick(REFRESH_RATE)
+
+
 def pvp_screen() -> None:
     """Hàm này là màn hình chơi giữa người với người"""
     # Tạo biến cho người chơi
@@ -1106,6 +1056,63 @@ def pvp_screen() -> None:
         pygame.display.flip()
 
         # Đợi đến frame tiếp theo
+        clock.tick(REFRESH_RATE)
+
+def main_menu():
+    """Hàm này là màn hình menu chính"""
+    # Tạo các nút
+    pve_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 220),
+                        text_input="Người vs Máy", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
+    pvp_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 330),
+                        text_input="Người vs Người", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
+    eve_button = Button(image=pygame.image.load("resources/button/normal_rect.png"), pos=(330.5, 440),
+                        text_input="Máy vs Máy", font=resource.get_font(30, 0), base_color="#AB001B", hovering_color="Black")
+    quit_button = Button(image=pygame.image.load("resources/button/small_rect.png"), pos=(330.5, 550),
+                         text_input="Thoát", font=resource.get_font(28, 0), base_color="Black", hovering_color="#AB001B")
+    # Bắt đầu vòng lặp game
+    while True:
+        # Nhận trạng thái trò chơi hiện tại
+        mouse_pos = pygame.mouse.get_pos()
+        events_list = pygame.event.get()
+
+        # Vẽ menu chính
+        # Vẽ background
+        bg_img, bg_pos = resource.background()
+        SCREEN.blit(bg_img, bg_pos)
+
+        # Vẽ chữ menu
+        menu_text = resource.get_font(100, 0).render("CỜ TƯỚNG", True, "White")
+        menu_rect = menu_text.get_rect(center = (330.5, 100))
+        SCREEN.blit(menu_text, menu_rect)
+
+        for button in [pve_button, pvp_button, eve_button, quit_button]:
+            button.draw(SCREEN)
+
+        # xử lý sự kiện
+        for event in events_list:
+            # Thoát khỏi trò chơi nếu nút QUIT được nhấp
+            if event.type == pygame.QUIT:
+                print("Thoát")
+                pygame.quit()
+                sys.exit()
+
+            # Xử lý sự kiện bấm vào các nút
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pve_button.check_for_input(mouse_pos):
+                    pve_menu()
+                if pvp_button.check_for_input(mouse_pos):
+                    pvp_menu()
+                if eve_button.check_for_input(mouse_pos):
+                    eve_menu()
+                if quit_button.check_for_input(mouse_pos):
+                    print("Thoát")
+                    pygame.quit()
+                    sys.exit()
+
+        # Cập nhật màn hình
+        pygame.display.flip()
+
+        # Chờ khung hình tiếp theo
         clock.tick(REFRESH_RATE)
 
 if __name__ == "__main__":
